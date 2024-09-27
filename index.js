5681e80a6c75203cc90bf647b162aa31700e213a
OM SONAWANE <omsonawane03@gmail.com>
2024-09-27 16:39:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function updateClock(){
+    const now =new Date();
+    let hours=now.getHours();
+    const meridiam = hours >=12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0);
+    const minutes=now.getMinutes().toString().padStart(2, 0);
+    const seconds=now.getSeconds().toString().padStart(2, 0); 
+    const timestring=`${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").textContent =timestring;
+}
+updateClock();
+setInterval(updateClock,1000);
